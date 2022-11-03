8a472aa3ae9a5314cc4a38f35bb103f03d029d18
Milovan99 <milovanjakovljevic78@gmail.com>
2022-11-03 23:11:32 +0100

    Promenjive

---
+++ src/main/kotlin/variable.kt
@@ -0,0 +1,8 @@
+fun variable(){
+    val ime : String ="Milovan"
+    var prezime : String ="Jakovljevic"
+//razlika izmedju val i var je da verdnost var promenjive moze biti izmenjena dok val se ne menja
+
+    prezime="Jakovljević"
+    println("$ime $prezime")
+}
